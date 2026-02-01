fe3b4a2ce66db713d695671bae1559f5464e35fe
aidyak <yusei.kaida@giftee.co>
2026-02-01 20:34:59 +0900

    クラッシュしても問題が起きないようにした

--- plugin/sentinel.lua
+++ plugin/sentinel.lua
@@ -1,1 +1,6 @@
-require("sentinel").setup()
+local ok, err = pcall(function()
+  require("sentinel").setup()
+end)
+if not ok then
+  vim.notify("Sentinel failed to load: " .. tostring(err), vim.log.levels.ERROR)
+end
